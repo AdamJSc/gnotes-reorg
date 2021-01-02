627e6c1b1dc44d3683371cb61b452469ec782e3c
Adam Scholey <hello@adamjs.net>
2021-01-02 15:53:39 +0000

    Add `run()`

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatalf("failed: %s", err.Error())
+	}
+}
+
+func run() error {
 	fmt.Println("hello world")
+	return nil
 }
